6261e06e52cb91c94e683f46618e02eb7e500595
unknown <wagnerras@gmail.com>
2021-02-27 21:48:18 -0300

    artigos

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,5 @@
 module.exports = app => {
+
     app.route('/users')
         .post(app.api.user.save)
         .get(app.api.user.get)
@@ -19,4 +20,17 @@ module.exports = app => {
         .get(app.api.category.getById)
         .put(app.api.category.save)
         .delete(app.api.category.remove)
+
+    app.route('/articles')
+        .get(app.api.article.get)
+        .post(app.api.article.save)
+
+    app.route('/articles/:id')
+        .get(app.api.article.getById)
+        .put(app.api.article.save)
+        .delete(app.api.article.remove)
+
+    app.route('/categories/:id/articles')
+        .get(app.api.article.getByCategory)
+
 }
